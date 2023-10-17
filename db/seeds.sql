eecaf24c30298af8db4d76ffaa059a7d02353031
booopyhij <mphillips2017@gmail.comgit config --global user.name booopyhijgit config --global user.email mphillips2017@gmail.com>
2023-10-17 03:12:45 -0400

    created the schema and seed

---
+++ db/seeds.sql
@@ -0,0 +1,8 @@
+INSERT INTO department (department_id, department_name)
+VALUES (1, 'tech'),
+        (2, 'sales'),
+        (3, 'Human resources'),
+        (4, 'customer service'),
+        (5, 'billing');
+
+INSERT INTO 
